2ae76e84a1cf06897a0eee286178eace340f18d2
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-05-28 22:26:04 -0700

    check if user have already register or not, if not then save user information in database and send all users to client side function added

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-// midlewire 
+// midlewire
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kgqetuh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,64 +18,72 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const menuCollection = client.db('bistroDb').collection('menu');
-    const cartCollection = client.db('bistroDb').collection('cart');
-    const usersCollection = client.db('bistroDb').collection('users');
+    const menuCollection = client.db("bistroDb").collection("menu");
+    const cartCollection = client.db("bistroDb").collection("cart");
+    const usersCollection = client.db("bistroDb").collection("users");
 
+    // users related APIs
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
-    // user information collect 
-    app.post('/users', async(req, res)=>{
+    app.post("/users", async (req, res) => {
       const body = req.body;
-
+      const query = { email: body.email };
+      const isExist = await usersCollection.findOne(query);
+      console.log(isExist);
+      if (isExist) {
+        return console.log("user is already exist");
+      }
       const result = await usersCollection.insertOne(body);
       res.send(result);
-    })
+    });
 
-
-    app.get('/menu', async(req, res)=>{
+    app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
-    })
+    });
 
-
-    // get cart 
-    app.get('/carts', async(req, res)=>{
+    // get cart
+    app.get("/carts", async (req, res) => {
       const email = req.query.email;
-      if(!email){
-        res.send([])
+      if (!email) {
+        res.send([]);
       }
-      
-      const query = {userEmail : email};
+
+      const query = { userEmail: email };
       const result = await cartCollection.find(query).toArray();
-      res.send(result)
-    })
-    // add to cart 
-    app.post('/carts',async(req, res)=>{
+      res.send(result);
+    });
+    // add to cart
+    app.post("/carts", async (req, res) => {
       const body = req.body;
       const result = await cartCollection.insertOne(body);
       res.send(result);
-    })
+    });
 
     // delete cart item
-    app.delete('/carts/:id', async(req , res)=>{
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
-      console.log({id})
-      const query = {_id : new ObjectId(id)};
+      console.log({ id });
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
-    })
-
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -85,14 +91,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Bistro Boss Resturant Server");
+});
 
-
-app.get('/', (req, res)=>{
-    res.send("Welcome to Bistro Boss Resturant Server")
-})
-
-
-
-app.listen(port, ()=>{
-    console.log(`bistro boss server is running on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`bistro boss server is running on port: ${port}`);
+});
